49d8c643b98448439a1a48cde652338443f7bf64
lamloi1109 <lamloi12a1@gmail.com>
2023-07-12 22:24:40 +0700

    Render PostList - loop through list - create li element from template - update textContent from post - calc relative time from now using dayjs - append child li element

---
+++ js/selectors/selectors.js
@@ -0,0 +1,6 @@
+export function getPostTemplateElement() {
+  return document.getElementById('postItemTemplate')
+}
+export function getPostListElement() {
+  return document.getElementById('postList')
+}
