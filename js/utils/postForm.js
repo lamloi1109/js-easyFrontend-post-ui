3f2b8374f53150b921d8980e1b9dfde6e97e49a5
lamloi1109 <lamloi12a1@gmail.com>
2023-07-27 20:03:32 +0700

    Feature Get form values: - Get form values. formData to json - Refactor code

--- js/utils/postForm.js
+++ js/utils/postForm.js
@@ -1,37 +1,69 @@
 import postAPI from '../api/postAPI.js'
+import { setBackgroundImage, setInputValue } from './common.js'
 
-function setFormValue(formData, datasetName, inputType) {
-  // title
-  const input = postForm.querySelector(`${inputType}[name='${datasetName}']`)
-  if (!input) return
-  input.value = formData ? formData[datasetName] : ''
-}
-
-export function initFormValue(formData, formName) {
-  if (!formData) return
-  const postForm = document.forms[formName]
-  if (!postForm) return
-
-  // title
-  setFormValue(formData, 'title', 'input')
-  // author
-  setFormValue(formData, 'author', 'input')
-  // desc
-  setFormValue(formData, 'description', 'textarea')
-
-  postForm.addEventListener('submit', (event) => {
-    handleFormSubmit(event, formData)
+function setFormValue(form, selector, formValue) {
+  setInputValue({
+    parent: form,
+    selector,
+    value: formValue,
   })
 }
 
-async function handleFormSubmit(event, formData) {
-  event.preventDefault()
-  // validation
+function setFormValues(form, formValues) {
+  if (!form) return
+  setFormValue(form, `input[name='title']`, formValues?.title)
+  // author
+  setFormValue(form, `input[name='author']`, formValues?.author)
+  // desc
+  setFormValue(form, `textarea[name='description']`, formValues?.description)
+  // imageURL
+  setFormValue(form, `input[name='imageUrl']`, formValues?.imageUrl)
+}
 
-  // edit or update
-  if (formData.id) {
-    console.log(formData)
-    // const res = await postAPI.update(formData)
-    // console.log(res)
+function getFormValues(form) {
+  const formValues = {}
+  // type formData to type json Data
+  // formData can't not accept input unchecked or disabled 
+  const data = new FormData(form)
+  for (const [key, value] of data) {
+    formValues[key] = value
   }
+  return formValues
+}
+
+export function initFormValue({ defaultValues, formName, onSubmit }) {
+  if (!defaultValues) return
+  const postForm = document.forms[formName]
+  if (!postForm) return
+
+  setFormValues(postForm, defaultValues)
+  // set background for heroImage
+  const postHeroImage = document.getElementById('postHeroImage')
+  if (postHeroImage) setBackgroundImage(postHeroImage, defaultValues?.imageUrl)
+
+  postForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    // get form values
+    const formValues = getFormValues(postForm)
+    console.log(formValues)
+    // validation
+    // using vanilla js
+    // String value
+    // check empty value
+    // check length
+    // check special character
+
+    // Image
+    // Check type image
+    // number of file
+    // Check width and height
+
+    // imageUrl -> Check url valid
+
+    // using yup
+
+    // if trigger submit CallBack
+    // otherwise
+    onSubmit?.(event, defaultValues)
+  })
 }
